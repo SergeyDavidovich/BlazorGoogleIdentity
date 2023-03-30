667ea0f996b5a823c0fe389dcf5e3a8caf64896e
Sergiusz Kriwobokow <writesd@hotmail.com>
2023-03-30 19:34:53 +0200

    no comments

--- BlazorGoogleIdentity/Program.cs
+++ BlazorGoogleIdentity/Program.cs
@@ -24,12 +24,21 @@ namespace BlazorGoogleIdentity
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            //builder.Services.AddOidcAuthentication(options =>
+            //{
+            //    // Configure your authentication provider options here.
+            //    // For more information, see https://aka.ms/blazor-standalone-auth
+            //    // https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.components.webassembly.authentication.oidcprovideroptions?view=aspnetcore-5.0
+            //    builder.Configuration.Bind("google", options.ProviderOptions);
+            //});
+
             builder.Services.AddOidcAuthentication(options =>
             {
-                // Configure your authentication provider options here.
-                // For more information, see https://aka.ms/blazor-standalone-auth
-                // https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.components.webassembly.authentication.oidcprovideroptions?view=aspnetcore-5.0
-                builder.Configuration.Bind("google", options.ProviderOptions);
+                options.ProviderOptions.Authority = "https://accounts.google.com/";
+                options.ProviderOptions.RedirectUri = "https://localhost:5001/authentication/login-callback";
+                options.ProviderOptions.PostLogoutRedirectUri = "https://localhost:5001/authentication/logout-callback";
+                options.ProviderOptions.ClientId = "589474797397-il6a6m8o5pc8idrdjrlmv8vka6m7nkmi.apps.googleusercontent.com";
+                options.ProviderOptions.ResponseType = "id_token";
             });
 
             await builder.Build().RunAsync();
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # BlazorGoogleIdentity
+### Project's links:
+https://chandradev819.wordpress.com/2021/03/22/google-authentication-and-authorization-in-blazor-webassembly/
+https://console.cloud.google.com/apis/credentials?project=api-6572864239938239592-574458
+https://stackoverflow.com/questions/66083740/google-auth-error-getting-access-token-in-blazor/66139736#66139736
+https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow
+
