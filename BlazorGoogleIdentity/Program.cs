68704443633c4377bae73ed869cd33e6731b1eda
Sergiusz Kriwobokow <writesd@hotmail.com>
2023-03-30 23:28:16 +0200

    no comments

--- BlazorGoogleIdentity/Program.cs
+++ BlazorGoogleIdentity/Program.cs
@@ -8,11 +8,6 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-// https://chandradev819.wordpress.com/2021/03/22/google-authentication-and-authorization-in-blazor-webassembly/
-// https://console.cloud.google.com/apis/credentials?project=api-6572864239938239592-574458
-// https://stackoverflow.com/questions/66083740/google-auth-error-getting-access-token-in-blazor/66139736#66139736
-// https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow
-
 namespace BlazorGoogleIdentity
 {
     public class Program
